0cadad60b133693d3e9a00796164294ae57002b3
Enim <odtenimp@gmail.com>
2025-11-20 15:00:45 +0800

    项目实战--准备阶段

---
+++ 04JavaWeb/05SSM/Practical Project/my-school-manager-springboot/src/main/java/com/bing/MySchoolManagerApplication.java
@@ -0,0 +1,14 @@
+package com.bing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySchoolManagerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MySchoolManagerApplication.class, args);
+    }
+
+}
---
+++ 04JavaWeb/05SSM/Practical Project/my-school-manager-springboot/src/test/java/com/bing/MySchoolManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySchoolManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
